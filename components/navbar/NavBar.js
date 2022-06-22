28f3b387130a37218680405b477b1ff5ab2c149e
conradotoptive <conradocastillo@toptive.co>
2022-06-22 10:00:50 -0300

    fixed some css problemas. Added collapse to navbar, but not working currently

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -20,12 +20,15 @@ const NavBar = (user) => {
 
     const handlePurchacesButtonClikc = () => {
         router.push('/purchaces');
-    }    
+    }  
 
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <button className="btn navbar-brand btn-link" onClick={handleProductsButtonClikc}>Products</button>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     {isUserLogged ?
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
